b281449795ebff0b300695058db52dac34d9bf19
23NateK <n.kimber22@gmail.com>
2023-02-20 12:36:37 -0600

    got copysquare and copyragged done maybe

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
   
+  public static int[][] copysquare(int[][] a){
+    int[][] b= new int[a.length][a[0].length];
+    for(int row=0;row<a.length;row++){
+      for(int val=0;val<a.length;val++){
+        b[row][val]=a[row][val];
+      }
+    }
+    return b;
+  }
+  
+  public static int[][] copyRagged(int[][] a){
+    int[][] b= new int[a.length][a[0].length];
+    for(int row=0;row<a.length;row++){
+      for(int val=0;val<a[0].length;val++){
+        b[row][val]=a[row][val];
+      }
+    }
+    return b;
+  }
+
+
   public static void main(String[] args) {
+  
     
   }
 }
